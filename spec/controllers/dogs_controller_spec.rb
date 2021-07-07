8cc730ce3f2f26422e6fcd40da40ce3b866e109e
Ryan Buskirk <rbuskirk@gmail.com>
2021-07-06 22:05:30 -0400

    add pagination to index and bootstap.

--- spec/controllers/dogs_controller_spec.rb
+++ spec/controllers/dogs_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe DogsController, type: :controller do
+
   describe '#index' do
     it 'displays recent dogs' do
       2.times { create(:dog) }
       get :index
       expect(assigns(:dogs).size).to eq(2)
     end
+
+    it 'by default returns maximum of 5 dogs' do
+      10.times { create(:dog) }
+      get :index
+      expect(assigns(:dogs).size).to eq(5)
+    end
+
   end
+
 end
